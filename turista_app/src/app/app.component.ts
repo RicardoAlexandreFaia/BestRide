083263bc75bb7c5b10dc77864f7e08bf2c8c850f
a21904553git config --global user.name a21904553git config --global user.name a21904553 <a21904553@alunos.ulht.pt>
2021-05-05 11:31:16 +0100

    globalização

--- turista_app/src/app/app.component.ts
+++ turista_app/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
+import { Globalization } from '@ionic-native/globalization/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private translate: TranslateService, private router: Router) {
+  constructor(private translate: TranslateService, private router: Router, private globalization: Globalization) {
     this.initializacao();
   }
 
   initializacao(): void {
-    // app tem como predefinicao a linguagem PT (meter liguagem do telemovel)
-    this.translate.setDefaultLang('pt');
+    this.globalization.getPreferredLanguage()
+      .then(res => console.log(res))
+      .catch(e => console.log(e));
   }
+  
 }
